7b470264b574e0073b4293e6ba1c294c1c096c67
Ivan Prystaia <greqit@gmail.com>
2024-06-28 17:39:18 +0300

    add BookMapperImpl

--- src/main/java/mate/academy/springbootwebgreqit/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/springbootwebgreqit/dto/CreateBookRequestDto.java
@@ -3,9 +3,15 @@ package mate.academy.springbootwebgreqit.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class CreateBookRequestDto {
     private String title;
     private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
 }
