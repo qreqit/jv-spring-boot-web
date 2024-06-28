7cc2b8c94da16e6faf68e3a1e23e1fcb016b31f2
Ivan Prystaia <greqit@gmail.com>
2024-06-28 19:21:08 +0300

    add annotations

--- src/main/java/mate/academy/springbootwebgreqit/model/Book.java
+++ src/main/java/mate/academy/springbootwebgreqit/model/Book.java
@@ -2,76 +2,29 @@ package mate.academy.springbootwebgreqit.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 100)
     private String title;
+    @Column(nullable = false, length = 50)
     private String author;
+    @Column(unique = true, length = 13)
     private String isbn;
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal price;
+    @Column(length = 2000)
     private String description;
+    @Column(name = "cover_image", length = 255)
     private String coverImage;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
 }
