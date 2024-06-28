41b86f7dd6a1f7d30a3100cd4576b23a5ddcfe0b
Ivan Prystaia <greqit@gmail.com>
2024-06-28 17:01:48 +0300

    start

---
+++ src/main/java/mate/academy/springbootwebgreqit/SpringBootWebGreqitApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.springbootwebgreqit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebGreqitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebGreqitApplication.class, args);
+	}
+
+}
