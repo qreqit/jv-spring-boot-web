4231e60425fcb0af8c291520f3d6b603a921cf7a
Ivan Prystaia <greqit@gmail.com>
2024-07-02 16:35:51 +0300

    fix

--- src/main/java/mate/academy/springbootwebgreqit/dto/UpdateBookRequestDto.java
+++ src/main/java/mate/academy/springbootwebgreqit/dto/UpdateBookRequestDto.java
@@ -9,19 +9,4 @@ public class UpdateBookRequestDto {
     private String title;
     private String author;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
--- src/main/java/mate/academy/springbootwebgreqit/model/Book.java
+++ src/main/java/mate/academy/springbootwebgreqit/model/Book.java
@@ -31,20 +31,4 @@ public class Book {
     private String coverImage;
     @Column(nullable = false)
     private boolean isDeleted = false;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
