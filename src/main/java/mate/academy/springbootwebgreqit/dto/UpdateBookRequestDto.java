647876c4dd9b784db220bb7457c8c0f64de030d7
Ivan Prystaia <greqit@gmail.com>
2024-07-02 13:07:57 +0300

    fix

--- src/main/java/mate/academy/springbootwebgreqit/dto/UpdateBookRequestDto.java
+++ src/main/java/mate/academy/springbootwebgreqit/dto/UpdateBookRequestDto.java
@@ -4,16 +4,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-
+@Getter
+@Setter
 public class UpdateBookRequestDto {
     private String title;
     private String author;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
 }
