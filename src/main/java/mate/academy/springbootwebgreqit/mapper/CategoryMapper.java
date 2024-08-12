9bb5030225677a29e2a4311ffd440f417bfbc338
Ivan Prystaia <greqit@gmail.com>
2024-08-12 20:47:49 +0200

    ..

--- src/main/java/mate/academy/springbootwebgreqit/mapper/CategoryMapper.java
+++ src/main/java/mate/academy/springbootwebgreqit/mapper/CategoryMapper.java
@@ -4,21 +4,20 @@ import mate.academy.springbootwebgreqit.dto.category.CategoryDto;
 import mate.academy.springbootwebgreqit.dto.category.UpdateCategoryRequestDto;
 import mate.academy.springbootwebgreqit.model.Category;
 import org.mapstruct.Mapper;
-import org.mapstruct.MapperConfig;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(componentModel = "spring")
 public interface CategoryMapper {
     @Mapping(source = "id", target = "id")
     @Mapping(source = "name", target = "name")
     @Mapping(source = "description", target = "description")
     CategoryDto toDto(Category category);
 
+    Category toEntity(CategoryDto categoryDTO);
+
     @Mapping(source = "id", target = "id")
     @Mapping(source = "name", target = "name")
     @Mapping(source = "description", target = "description")
-    Category toEntity(CategoryDto categoryDTO);
-
-    void updateCategoryFromDto(UpdateCategoryRequestDto book, @MappingTarget Category entity);
+    void updateCategoryFromDto(UpdateCategoryRequestDto categoryDto, @MappingTarget Category entity);
 }
