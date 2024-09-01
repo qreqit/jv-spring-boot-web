029c17be1a5442ef3b2c9ff743676f90b76e25c4
greqit <greqit@gmail.com>
2024-09-01 14:29:12 +0200

    fix spases in book class

--- src/main/java/mate/academy/springbootwebgreqit/model/Book.java
+++ src/main/java/mate/academy/springbootwebgreqit/model/Book.java
@@ -1,53 +1,60 @@
-    package mate.academy.springbootwebgreqit.model;
+package mate.academy.springbootwebgreqit.model;
 
-    import jakarta.persistence.*;
-    import jakarta.validation.constraints.Min;
-    import jakarta.validation.constraints.NotBlank;
-    import jakarta.validation.constraints.NotNull;
-    import lombok.EqualsAndHashCode;
-    import lombok.Getter;
-    import lombok.Setter;
-    import org.apache.commons.lang3.builder.ToStringExclude;
-    import org.hibernate.annotations.SQLDelete;
-    import org.hibernate.annotations.SQLRestriction;
-    import java.math.BigDecimal;
-    import java.util.HashSet;
-    import java.util.Set;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 
-    @Getter
-    @Setter
-    @Entity
-    @SQLDelete(sql = "UPDATE books SET is_deleted = true where id = ?")
-    @SQLRestriction("is_deleted = false")
-    @Table(name = "books")
-    public class Book {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        @Column(nullable = false, length = 100)
-        private String title;
-        @Column(nullable = false, length = 50)
-        private String author;
-        @Column(nullable = false, unique = true, length = 13)
-        private String isbn;
-        @Column(nullable = false, precision = 10, scale = 2)
-        @Min(0)
-        private BigDecimal price;
-        @Column(nullable = false, length = 2000)
-        private String description;
-        @Column(nullable = false, length = 255)
-        private String coverImage;
-        @ManyToMany(fetch = FetchType.EAGER)
-        @JoinTable(
-                name = "books_categories",
-                joinColumns = @JoinColumn(name = "book_id"),
-                inverseJoinColumns = @JoinColumn(name = "category_id")
-        )
-        @ToStringExclude
-        @EqualsAndHashCode.Exclude
-        private Set<Category> categories = new HashSet<>();
-        private boolean isDeleted = false;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
-        @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-        private Set<OrderItem> orderItems;
-    }
+@Getter
+@Setter
+@Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String author;
+
+    @Column(nullable = false, unique = true, length = 13)
+    private String isbn;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    @Min(0)
+    private BigDecimal price;
+
+    @Column(nullable = false, length = 2000)
+    private String description;
+
+    @Column(nullable = false, length = 255)
+    private String coverImage;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "books_categories",
+        joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    @ToStringExclude
+    @EqualsAndHashCode.Exclude
+    private Set<Category> categories = new HashSet<>();
+
+    private boolean isDeleted = false;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<OrderItem> orderItems;
+}
