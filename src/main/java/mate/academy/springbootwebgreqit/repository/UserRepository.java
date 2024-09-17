c7bb5aa52f065f2985871b0ad27aa662b25b9bc6
greqit <greqit@gmail.com>
2024-09-17 17:50:05 +0200

    add .env file

--- src/main/java/mate/academy/springbootwebgreqit/repository/UserRepository.java
+++ src/main/java/mate/academy/springbootwebgreqit/repository/UserRepository.java
@@ -11,7 +11,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
-    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(String email);
-
 }
