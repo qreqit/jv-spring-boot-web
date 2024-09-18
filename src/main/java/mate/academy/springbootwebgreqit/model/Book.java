4ee3ad4b0118a553cf322292e2e1bf5acb34c8c6
greqit <greqit@gmail.com>
2024-09-18 18:30:32 +0200

    chanfe sql file and use tostring.exclude

--- src/main/java/mate/academy/springbootwebgreqit/model/Book.java
+++ src/main/java/mate/academy/springbootwebgreqit/model/Book.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Min;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.apache.commons.lang3.builder.ToStringExclude;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
@@ -52,7 +53,7 @@ public class Book {
         joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns = @JoinColumn(name = "category_id")
     )
-    @ToStringExclude
+    @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private Set<Category> categories = new HashSet<>();
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
+-- Ensure the roles table exists
+CREATE TABLE IF NOT EXISTS roles (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Insert roles
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
