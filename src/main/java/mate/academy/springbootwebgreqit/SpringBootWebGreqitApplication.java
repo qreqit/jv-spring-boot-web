4f496da0f2a88743ae77eebf2d2bf0f773cdb892
greqit <greqit@gmail.com>
2024-09-18 18:21:20 +0200

    fix

--- src/main/java/mate/academy/springbootwebgreqit/SpringBootWebGreqitApplication.java
+++ src/main/java/mate/academy/springbootwebgreqit/SpringBootWebGreqitApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "mate.academy.springbootwebgreqit.repository")
 public class SpringBootWebGreqitApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebGreqitApplication.class, args);
