65b5eabcbf9016e666dc438a9a29a8b5b8719499
greqit <greqit@gmail.com>
2024-09-20 18:56:17 +0200

    delete unused imports

--- src/main/java/mate/academy/springbootwebgreqit/security/AuthenticationService.java
+++ src/main/java/mate/academy/springbootwebgreqit/security/AuthenticationService.java
@@ -1,13 +1,10 @@
 package mate.academy.springbootwebgreqit.security;
 
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.springbootwebgreqit.dto.user.UserLoginRequestDto;
 import mate.academy.springbootwebgreqit.dto.user.UserLoginResponseDto;
-import mate.academy.springbootwebgreqit.model.User;
 import mate.academy.springbootwebgreqit.repository.UserRepository;
-import org.hibernate.Hibernate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,9 +19,6 @@ public class AuthenticationService {
 
     @Transactional
     public UserLoginResponseDto authenticate(UserLoginRequestDto request) {
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new EntityNotFoundException("User not found"));
-        Hibernate.initialize(user.getRoles());
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
         );
