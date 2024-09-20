f3cddf07f6ec52accc415545b6dde7063b2ad227
greqit <greqit@gmail.com>
2024-09-20 18:37:52 +0200

    fix lazy initialize in  registration case and fix dockeer

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 # Builder stage
-FROM openjdk:17-jdk-alpine AS builder
-WORKDIR /application
+FROM openjdk:17-jdk-slim as builder
+WORKDIR application
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} application.jar
 RUN java -Djarmode=layertools -jar application.jar extract
-
 # Final stage
-FROM openjdk:17-jdk-alpine
-WORKDIR /application
-COPY target/*.jar application.jar
-ENTRYPOINT ["java", "-jar", "application.jar"]
+FROM openjdk:17-jdk-slim
+WORKDIR applicationr
+COPY --from=builder application/dependencies/ ./
+COPY --from=builder application/spring-boot-loader/ ./
+COPY --from=builder application/snapshot-dependencies/ ./
+COPY --from=builder application/application/ ./
+ENTRYPOINT ["java", "org.springframework.boot.loader.launch.JarLauncher"]
 EXPOSE 8080
--- src/main/java/mate/academy/springbootwebgreqit/security/AuthenticationService.java
+++ src/main/java/mate/academy/springbootwebgreqit/security/AuthenticationService.java
@@ -1,8 +1,13 @@
 package mate.academy.springbootwebgreqit.security;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.springbootwebgreqit.dto.user.UserLoginRequestDto;
 import mate.academy.springbootwebgreqit.dto.user.UserLoginResponseDto;
+import mate.academy.springbootwebgreqit.model.User;
+import mate.academy.springbootwebgreqit.repository.UserRepository;
+import org.hibernate.Hibernate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,8 +18,13 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
+    @Transactional
     public UserLoginResponseDto authenticate(UserLoginRequestDto request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+        Hibernate.initialize(user.getRoles());
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
         );
