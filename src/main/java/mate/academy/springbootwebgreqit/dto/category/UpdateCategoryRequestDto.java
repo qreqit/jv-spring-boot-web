0982a72df85457bf8c8bb7642f445f9bc738fce9
greqit <greqit@gmail.com>
2024-10-01 10:48:31 +0200

    add ci.yaml file

--- src/main/java/mate/academy/springbootwebgreqit/dto/category/UpdateCategoryRequestDto.java
+++ src/main/java/mate/academy/springbootwebgreqit/dto/category/UpdateCategoryRequestDto.java
@@ -1,7 +1,6 @@
 package mate.academy.springbootwebgreqit.dto.category;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
